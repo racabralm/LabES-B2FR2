2f621a3f08011b28ac0e95d1cce9c353605a43bc
FabyaniYan <yan.fabyani@gmail.com>
2026-09-19 18:34:25 -0300

    classe dominio

--- backend/programa_social.py
+++ backend/programa_social.py
@@ -1,1 +1,14 @@
+class ProgramaSocial:
+    def __init__(self, nome, exige_cadastro_unico, link_oficial):
+        self.nome = nome
+        self.exige_cadastro_unico = exige_cadastro_unico
+        self.link_oficial = link_oficial
 
+    def possui_cadastro_unico(self):
+        return self.exige_cadastro_unico
+
+    def possui_link_oficial(self):
+        return self.link_oficial.startswith("https://")
+
+    def nome_corresponde_a_busca(self, termo):
+        return termo.lower() in self.nome.lower()
